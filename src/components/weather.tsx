e7d11c7869019ff1187d48c4f02e78442aac2930
fkalapish <fkalapish@ethz.ch>
2023-06-10 14:19:12 +0200

    minor

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,9 +1,51 @@
 import { Title } from "@mantine/core"
+import { useEffect, useState } from "react"
 
-function Weather(){
-    return(
+function Weather() {
+    //vars & func
+    function api<T>(url: string): Promise<T> {
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText)
+                }
+                return response.json().then(data => data as T);
+            })
+    }
+
+    //get version
+    const [curr_temp, setTemp] = useState(100)
+
+    //runs only on first render
+    useEffect(() => {
+        var x = 'https://www.meteoswiss.admin.ch/product/output/versions.json';
+            import(x).then((a) => {
+                a.json()
+                console.log(a.keys())
+            });
+    }, []);
+
+    //runs periodically every 50seconds (not on first render)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            //const url = 'https://www.meteoswiss.admin.ch/product/output/versions.json';
+
+            //import * as versions from 'https://www.meteoswiss.admin.ch/product/output/versions.json';
+
+            var x = 'https://www.meteoswiss.admin.ch/product/output/versions.json';
+            import(x).then((a) => {
+                a.json()
+                console.log(a.keys())
+            });
+
+        }, 300000); //every 5mins
+        return () => clearInterval(interval); //unmount to prevent memory leaks.
+    }, [])
+
+    return (
         <>
         <Title>Weather</Title>
+            <Title>{curr_temp}</Title>
         </>
     )
 }
